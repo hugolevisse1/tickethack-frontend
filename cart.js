01d6d2b391c04be1b09a5817ab6d1971e97b7f54
Marion <marionlaurin@hotmail.com>
2023-04-12 09:52:57 +0200

    last front

--- cart.js
+++ cart.js
@@ -1,13 +1,23 @@
-function deleteTripListener() {
-	for (let i = 0; i < document.querySelectorAll('#btn-delete').length; i++) {
-		document.querySelectorAll('#btn-delete')[i].addEventListener('click', function () {
-			fetch(`http://localhost:3000/trips/${this.id}`, { method: 'DELETE' })
-				.then(response => response.json())
-				.then(data => {
-					if (data.result) {
-						this.parentNode.remove();
-					}
-				});
-		});
-	}
-}
+// fetch(`http://localhost:3000/trips/`)
+// 	.then 
+
+// // function addCarts() {
+// // }
+// affiche tous les trajets ajoutés au panier avec la possibilité de les supprimer.
+
+// function deleteTripListener() {
+// 	for (let i = 0; i < document.querySelectorAll('.btn-delete').length; i++) {
+// 		document.querySelectorAll('.btn-delete')[i].addEventListener('click', function () {
+// 			fetch(`http://localhost:3000/trips/${this.id}`, { method: 'DELETE' })
+// 				.then(response => response.json())
+// 				.then(data => {
+// 					console.log(data)
+// 					if (data.result) {
+// 						this.parentNode.parentNode.remove();
+// 					}
+// 				});
+// 		});
+// 	}
+// }
+
+// deleteTripListener()
