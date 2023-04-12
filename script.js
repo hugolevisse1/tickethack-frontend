64cfe7698817ed4241abf10ab28b386737a07f24
Marion <marionlaurin@hotmail.com>
2023-04-12 10:12:14 +0200

    pull

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-
+// rechercher les trajets depuis home
+document.querySelector('#btn-search').addEventListener('click', function () {
+    const departure = document.querySelector('#search').value
+    const arrival = document.querySelector('#search').value
+    console.log(departure)
+})
